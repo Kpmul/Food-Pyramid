32f21e62284239b16483b7b6da723e09d21cd85a
Karl Mulraney <116092530+Kpmul@users.noreply.github.com>
2023-03-29 14:41:50 +0100

    Add files via upload

---
+++ Food Pyramid/Assignment-01.js
@@ -0,0 +1,122 @@
+
+
+function incrementRed(){
+    var element = document.getElementById('red');
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('red').innerHTML = value;
+    }
+function decrementRed(){
+    var element = document.getElementById('red');
+    var value = element.innerHTML;
+
+    if(value >= 1)
+    --value;
+    
+
+    console.log(value);
+    document.getElementById('red').innerHTML = value;
+}
+
+function incrementOrange(){
+    var element = document.getElementById('orange');
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('orange').innerHTML = value;
+    }
+function decrementOrange(){
+    var element = document.getElementById('orange');
+    var value = element.innerHTML;
+    if(value >=1)
+    --value;
+
+    console.log(value);
+    document.getElementById('orange').innerHTML = value;
+}
+
+function incrementYellow(){
+    var element = document.getElementById('yellow');
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('yellow').innerHTML = value;
+    }
+
+function decrementYellow(){
+    var element = document.getElementById('yellow');
+    var value = element.innerHTML;
+
+    if(value>=1)
+    --value;
+
+    console.log(value);
+    document.getElementById('yellow').innerHTML = value;
+}
+
+function incrementBlue(){
+    var element = document.getElementById('blue');
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('blue').innerHTML = value;
+    }
+function decrementBlue(){
+    var element = document.getElementById('blue');
+    var value = element.innerHTML;
+
+    if(value >= 1)
+    --value;
+
+    console.log(value);
+    document.getElementById('blue').innerHTML = value;
+}
+
+function incrementGreen(){
+    var element = document.getElementById('green');
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('green').innerHTML = value;
+    }
+function decrementGreen(){
+    var element = document.getElementById('green');
+    var value = element.innerHTML;
+
+    if(value >= 1)
+    --value;
+
+    console.log(value);
+    document.getElementById('green').innerHTML = value;
+}
+
+function incrementBrown(){
+    var element = document.getElementById('brown');
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('brown').innerHTML = value;
+    }
+function decrementBrown(){
+    var element = document.getElementById('brown');
+    var value = element.innerHTML;
+
+    if(value >= 1)
+    --value;
+
+    console.log(value);
+    document.getElementById('brown').innerHTML = value;
+}
